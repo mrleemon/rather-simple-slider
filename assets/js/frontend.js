c10937a4efa7d6a83fb60d1a9378f46ce0add961
leemon <tacrapo@gmail.com>
2020-12-06 18:51:38 +0100

    Remove deprecated ready() syntax

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,27 +1,32 @@
-jQuery( document ).ready( function( $ ) {
+( function( $ ) {
+
+    $( function() {
     
-    $( '.slider' ).each( function() {
+        $( '.slider' ).each( function() {
 
-        var slider = $( this );
+            var slider = $( this );
 
-        $( '.slider-items', slider ).slick();
+            $( '.slider-items', slider ).slick();
 
-        $( '.toggle-text', slider ).on( 'click', function() {
-            $( '.slider-text', slider ).show();
-            $( '.slider-navigation', slider ).hide();
-            $( '.slider-items', slider ).hide();
-            $( this, slider ).hide();
-            $( '.toggle-media', slider ).show();
+            $( '.toggle-text', slider ).on( 'click', function() {
+                $( '.slider-text', slider ).show();
+                $( '.slider-navigation', slider ).hide();
+                $( '.slider-items', slider ).hide();
+                $( this, slider ).hide();
+                $( '.toggle-media', slider ).show();
+            } );
+        
+            $( '.toggle-media', slider ).on( 'click', function() {
+                $( '.slider-text', slider ).hide();
+                $( '.slider-navigation', slider ).show();
+                $( '.slider-items', slider ).show();
+                $( this, slider ).hide();
+                $( '.toggle-text', slider ).show();
+            } );
+        
         } );
-    
-        $( '.toggle-media', slider ).on( 'click', function() {
-            $( '.slider-text', slider ).hide();
-            $( '.slider-navigation', slider ).show();
-            $( '.slider-items', slider ).show();
-            $( this, slider ).hide();
-            $( '.toggle-text', slider ).show();
-        } );
-    
+
     } );
 
-} );
+} )( jQuery );
+
