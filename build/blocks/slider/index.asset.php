e95454c98133181981fde0af8d30d76ae8c05b30
leemon <tacrapo@gmail.com>
2023-05-12 15:04:45 +0200

    Remove commented code and rebuild

--- build/blocks/slider/index.asset.php
+++ build/blocks/slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '23cd161bc6b51e3cb6ca');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f81da0bc0dc22b81950b');
--- src/blocks/slider/index.js
+++ src/blocks/slider/index.js
@@ -26,24 +26,8 @@ const { name } = metadata;
  * Internal dependencies
  */
 
-/*const blockAttributes = {
-    id: {
-        type: 'integer',
-        default: 0,
-    },
-};*/
-
-//export const name = 'occ/rather-simple-slider';
 
 export const settings = {
-    /*
-	title: __( 'Rather Simple Slider', 'rather-simple-slider' ),
-    description: __( 'Display a slider.', 'rather-simple-slider' ),
-    category: 'common',
-    keywords: [ __( 'images', 'rather-simple-slider' ), __( 'photos', 'rather-simple-slider' ) ],
-    attributes: blockAttributes,
-	*/
-
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><G><Path d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 9.67l1.69 2.26 2.48-3.1L19 15H9zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z" /></G></SVG>,
     
     edit: ( props => {
